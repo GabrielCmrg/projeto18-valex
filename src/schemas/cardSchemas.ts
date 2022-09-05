f94bedf494d412d8b46929c127be4ef15a055278
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-05 17:31:11 -0300

    feat: add new schema for card activation

--- src/schemas/cardSchemas.ts
+++ src/schemas/cardSchemas.ts
@@ -6,9 +6,15 @@ export const newCardSchema = Joi.object({
     .trim()
     .required()
     .allow('groceries', 'restaurants', 'transport', 'education', 'health'),
-  employeeId: Joi.number().integer().greater(0),
+  employeeId: Joi.number().integer().greater(0).required(),
 });
 
 export const APIKeySchema = Joi.object({
   'x-api-key': Joi.string().trim().required()
 }).unknown(true);
+
+export const activateCardSchema = Joi.object({
+  cardId: Joi.number().integer().greater(0).required(),
+  securityCode: Joi.string().trim().required().pattern(/^[0-9]{3}$/),
+  password: Joi.string().trim().required(),
+});
